a448d6a3afad95fffba8b54b13104932e37a9669
dmcgann1 <dmcgann1@gmail.com>
2014-09-12 14:43:34 +0100

    Reviews are now limited to 1 per user per facility. Only authors can edit/delete and only other users can vote on reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,7 @@ class Review < ActiveRecord::Base
   belongs_to :facility
 
   validates :body, presence: true
+  validates_uniqueness_of :user_id, scope: :facility_id
 
   def score
     score = votes.where(is_up: true).size - votes.where(is_up: false).size
